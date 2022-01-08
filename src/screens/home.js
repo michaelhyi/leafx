a47fe9fd630f99a93f9a8b817b67af760873b6e4
23yimichael <23yimichael@gmail.com>
2022-01-07 17:16:41 -0800

    image picker implemented

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,29 @@
-import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import Constants from "expo-constants";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Home() {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <TouchableOpacity onPress={pickImage}>
+        <Text>Pick an image</Text>
+      </TouchableOpacity>
     </View>
   );
 }
